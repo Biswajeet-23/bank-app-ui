0f235f3bab5c7ecdb7a6f1edb63c066dcfc9039e
Biswajeet Sahoo <2000biswajeet@gmail.com>
2023-10-17 09:30:49 +0530

    initial commit

---
+++ lib/data_json/card_operations_json.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_font_icons/flutter_font_icons.dart';
+
+const List cardOperations = [
+  {
+    "icon" : AntDesign.creditcard,
+    "title" : "Top up card"
+  },
+  {
+    "icon" : AntDesign.wallet,
+    "title" : "Payments"
+  },
+  {
+    "icon" : AntDesign.arrowright,
+    "title" : "Card output"
+  },
+  {
+    "icon" : AntDesign.creditcard,
+    "title" : "Take all the money from the card"
+  }
+];
---
+++ lib/pages/root_app.dart
@@ -0,0 +1,101 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:flutter_font_icons/flutter_font_icons.dart';
+import 'package:mobile_bank_app/pages/dashboard_page.dart';
+import 'package:mobile_bank_app/theme/color.dart';
+import 'package:flutter/material.dart';
+
+class RootApp extends StatefulWidget {
+  const RootApp({Key? key}) : super(key: key);
+
+  @override
+  State<RootApp> createState() => _RootAppState();
+}
+
+class _RootAppState extends State<RootApp> {
+  int pageIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: white,
+        bottomNavigationBar: getFooter(),
+        body: getBody(),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              selectedTab(4);
+            },
+            child: Icon(
+              AntDesign.creditcard,
+              size: 25,
+            ),
+            backgroundColor: primary
+            //params
+            ),
+        floatingActionButtonLocation:
+            FloatingActionButtonLocation.centerDocked);
+  }
+
+  Widget getBody() {
+    return IndexedStack(
+      index: pageIndex,
+      children: [
+        DashbaordPage(),
+        Center(
+          child: Text("Chat Page",style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold
+          ),),
+        ),
+        Center(
+          child: Text("Notification Page",style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold
+          ),),
+        ),
+        Center(
+          child: Text("Account Page",style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold
+          ),),
+        ),
+        Center(
+          child: Text("Card Page",style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold
+          ),),
+        ),
+      ],
+    );
+  }
+
+  Widget getFooter() {
+    List<IconData> iconItems = [
+      MaterialCommunityIcons.view_grid,
+      MaterialCommunityIcons.comment,
+      MaterialCommunityIcons.bell,
+      MaterialCommunityIcons.account_circle,
+    ];
+
+    return AnimatedBottomNavigationBar(
+      activeColor: primary,
+      splashColor: secondary,
+      inactiveColor: Colors.black.withOpacity(0.5),
+      icons: iconItems,
+      activeIndex: pageIndex,
+      gapLocation: GapLocation.center,
+      notchSmoothness: NotchSmoothness.softEdge,
+      leftCornerRadius: 10,
+      iconSize: 25,
+      rightCornerRadius: 10,
+      onTap: (index) {
+        selectedTab(index);
+      },
+      //other params
+    );
+  }
+
+  selectedTab(index) {
+    setState(() {
+      pageIndex = index;
+    });
+  }
+}
